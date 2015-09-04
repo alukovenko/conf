ebeaaf9735a3ed2d329e89adafe03fd0e72b31d5
Andrei Lukovenko <andrei.lukovenko@haufe.com>
2015-09-04 16:03:23 +0200

    agent save

--- genpwd.pl
+++ genpwd.pl
@@ -1,12 +1,11 @@
 #!/usr/bin/perl
 
 use utf8;
-use 5.014;
 use warnings;
 
 my @chars = ('0'..'9', 'A'..'Z', 'a'..'z');
 for (0..9) {
     my $length = $ARGV[1] || 8;
     while ($length--) { print $chars[int(rand @chars)] }
-    say '';
+    print "\n";
 }
